61151e0e0d17f87bf0d90fdfe26b7a946b5bdd2d
KoerlinF <104910480+KoerlinF@users.noreply.github.com>
2022-06-26 18:03:21 +0200

    Interfaces haben keine Attribute --- Attribute in Klassen verschoben --- --> Abstrakte Klasse einführen, die Interfaces ersetzt ?!

--- src/main/java/OtherClasses/Karte.java
+++ src/main/java/OtherClasses/Karte.java
@@ -1,9 +1,28 @@
 package OtherClasses;
 
+import java.util.Date;
+
 public class Karte implements KartenStrukturobjekt{
+    private String titel = null;
+    private Date erstelldatum = null;
+    private String modul = null;
+    private String ersteller = null;
     private boolean kannIch;
     private String vorderseite;
     private String rueckseite;
+
+    //Konstruktor
+
+
+    public Karte(String titel, Date erstelldatum, String modul, String ersteller, String vorderseite, String rueckseite) {
+        this.titel = titel;
+        this.erstelldatum = erstelldatum;
+        this.modul = modul;
+        this.ersteller = ersteller;
+        this.kannIch = false;
+        this.vorderseite = vorderseite;
+        this.rueckseite = rueckseite;
+    }
 
     @Override
     public void erstellen() {
--- src/main/java/OtherClasses/KartenStrukturobjekt.java
+++ src/main/java/OtherClasses/KartenStrukturobjekt.java
@@ -1,6 +1,5 @@
 package OtherClasses;
 
 public interface KartenStrukturobjekt extends Strukturobjekt{
-    String modul = null;
-    String ersteller = null;
+
 }
